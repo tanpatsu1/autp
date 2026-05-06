a761ea375b36d5b54562c92c8d446a545aa6493e
ロン毛 <feilianghexi@gmail.com>
2026-05-06 18:20:59 +0900

    Delete app directory

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { isSupabaseConfigured } from "@/lib/supabase/client";
-import { SavedUrlManager } from "./saved-url-manager";
-
-export default function Home() {
-  return <SavedUrlManager isSupabaseConfigured={isSupabaseConfigured} />;
-}
